3e3ad07e7f11eb4ddf774e7c2097a8c2763007b7
Skobayashi <suguru@iii-planning.com>
2011-11-21 13:35:31 +0900

    today edited.

--- public_html/resources/lunch-pack/js/window.js
+++ public_html/resources/lunch-pack/js/window.js
@@ -5,7 +5,6 @@ lunch.window = {
         var el = me.generateBaseElement('info');
 
         var paragraph = document.createElement('p');
-        paragraph.setAttribute('style', 'float:left;');
         paragraph.innerHTML = titleValue;
 
         var t = document.getElementById('lunch-window-title-info');
@@ -20,6 +19,94 @@ lunch.window = {
         var height = el.offsetHeight / 2;
         el.setAttribute('style', 'margin-top: -' + height + 'px;');
 
+        var button = document.createElement('input'),
+            b_id = lunch.core.genId();
+        button.setAttribute('id', b_id);
+        button.setAttribute('type', 'button');
+        button.setAttribute('value', 'OK');
+        button.setAttribute('class', 'lunch-button-success');
+        button.setAttribute('style', 'margin:10px;margin-left: 325px;padding-left:20px;padding-right:20px;');
+        button.setAttribute('onmouseup', 'lunch.window.hide();');
+
+        el.appendChild(button);
+
+    },
+
+    alert: function (titleValue, msgValue) {
+        var me = this,
+            el = me.generateBaseElement('alert');
+
+        var paragraph = document.createElement('p');
+        paragraph.innerHTML = titleValue;
+
+        var t = document.getElementById('lunch-window-title-alert');
+        t.appendChild(paragraph);
+
+        var msg = document.createElement('p');
+        msg.innerHTML = msgValue;
+
+        var m = document.getElementById('lunch-window-msg');
+        m.appendChild(msg);
+
+        var height = el.offsetHeight / 2;
+        el.setAttribute('style', 'margin-top: -' + height + 'px;');
+
+        var button = document.createElement('input'),
+            b_id = lunch.core.genId();
+        button.setAttribute('id', b_id);
+        button.setAttribute('type', 'button');
+        button.setAttribute('value', 'OK');
+        button.setAttribute('class', 'lunch-button-caution');
+        button.setAttribute('style', 'margin:10px;margin-left: 325px;padding-left:20px;padding-right:20px;');
+        button.setAttribute('onmouseup', 'lunch.window.hide();');
+
+        el.appendChild(button);
+        
+    },
+
+    confirm: function (titleValue, msgValue, func) {
+        var me = this,
+            el = me.generateBaseElement('confirm');
+
+        var paragraph = document.createElement('p');
+        paragraph.innerHTML = titleValue;
+
+        var t = document.getElementById('lunch-window-title-confirm');
+        t.appendChild(paragraph);
+
+        var msg = document.createElement('p');
+        msg.innerHTML = msgValue;
+
+        var m = document.getElementById('lunch-window-msg');
+        m.appendChild(msg);
+
+        var height = el.offsetHeight / 2;
+        el.setAttribute('style', 'margin-top: -' + height + 'px;');
+
+        var button = document.createElement('input'),
+            negative = document.createElement('input'),
+            buttons = document.createElement('div');
+        
+        buttons.setAttribute('style', 'float:left;margin-left: 211px;');
+
+        button.setAttribute('id', lunch.core.genId());
+        button.setAttribute('type', 'button');
+        button.setAttribute('value', 'OK');
+        button.setAttribute('class', 'lunch-button-info');
+        button.setAttribute('style', 'margin:10px;padding-left:20px;padding-right:20px;');
+        button.setAttribute('onmouseup', func);
+
+        negative.setAttribute('id', lunch.core.genId());
+        negative.setAttribute('type', 'button');
+        negative.setAttribute('value', 'Cancel');
+        negative.setAttribute('class', 'lunch-button-info');
+        negative.setAttribute('style', 'margin:10px;margin-left:5px;padding-left:20px;padding-right:20px;');
+        negative.setAttribute('onmouseup', 'lunch.window.hide();');
+
+        buttons.appendChild(button);
+        buttons.appendChild(negative);
+
+        el.appendChild(buttons);
     },
 
     generateBaseElement: function (type) {
@@ -42,18 +129,8 @@ lunch.window = {
         var msg = document.createElement('div');
         msg.setAttribute('id', 'lunch-window-msg');
 
-        var button = document.createElement('input'),
-            b_id = lunch.core.genId();
-        button.setAttribute('id', b_id);
-        button.setAttribute('type', 'button');
-        button.setAttribute('value', 'OK');
-        button.setAttribute('class', 'lunch-button-success');
-        button.setAttribute('style', 'margin:10px;margin-left: 325px;padding-left:20px;padding-right:20px;');
-        button.setAttribute('onmouseup', 'lunch.window.hide();');
-
         wrap.appendChild(title);
         wrap.appendChild(msg);
-        wrap.appendChild(button);
 
         el.appendChild(mask);
         el.appendChild(wrap);
